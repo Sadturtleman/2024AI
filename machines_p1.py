459c42cd3a64455044ecc2ec7744ef49255cb548
Sadturtleman <147583495+Sadturtleman@users.noreply.github.com>
2024-12-06 16:22:26 +0900

    add minimax alg

--- machines_p1.py
+++ machines_p1.py
@@ -1,34 +1,68 @@
 import numpy as np
 import random
 from itertools import product
-
-import time
+import tool
 
 class P1():
     def __init__(self, board, available_pieces):
         self.pieces = [(i, j, k, l) for i in range(2) for j in range(2) for k in range(2) for l in range(2)]  # All 16 pieces
         self.board = board # Include piece indices. 0:empty / 1~16:piece
         self.available_pieces = available_pieces # Currently available pieces in a tuple type (e.g. (1, 0, 1, 0))
-    
+
+
     def select_piece(self):
-        # Make your own algorithm here
- # Check time consumption (Delete when you make your algorithm)
 
-        return random.choice(self.available_pieces)
-
-    def place_piece(self, selected_piece):
-        # selected_piece: The selected piece that you have to place on the board (e.g. (1, 0, 1, 0)).
-        
-        # Available locations to place the piece
         available_locs = [(row, col) for row, col in product(range(4), range(4)) if self.board[row][col]==0]
 
-        # Make your own algorithm here
- # Check time consumption (Delete when you make your algorithm)
+        notcheckmateposition = tool.find_notcheckmate_piece(self.board, self.available_pieces, available_locs)
 
-        return random.choice(available_locs)
+        if bool(notcheckmateposition):
+            self.available_pieces = notcheckmateposition
+
+        #기보가 level 6(16 - 8) 전까진 랜덤으로 진행        
+        if len(available_locs) >= 12:
+            return random.choice(self.available_pieces)
+
+        piece, _ = tool.minimax(self.board, self.available_pieces, available_locs)
+        
+        return piece
+
+    def place_piece(self, selected_piece):
+
+        available_locs = [(row, col) for row, col in product(range(4), range(4)) if self.board[row][col]==0]
+
+        checkmate_position = tool.find_checkmate_place(self.board, selected_piece, available_locs)
+        if bool(checkmate_position):
+            return checkmate_position[0]
+
+        # 기보가 level (16 - 10) 전까진 랜덤으로 진행
+        if len(available_locs) >= 12:
+            return random.choice(available_locs)
+        
+        data = tool.find_three(self.board)
+
+        for index, (r, c) in enumerate(available_locs):
+            self.board[r][c] = self.pieces.index(selected_piece) + 1
+            if tool.check_win(self.board):
+                for temp in data:
+                    if temp[1][0] == r and temp[1][1] == c:
+                        num = tool.find_piece_by_point(self.board, self.available_pieces)[index]
+
+                        if num % 2 == 0:
+                            return (r, c)
+                        
+                        else:
+                            available_locs[index] = (None, None)
+
+            self.board[r][c] = 0
+
+        # N이 3개 있을 때 S를 받은 경우 S가 짝수개 있으면 N을 막는 위치
+        # N이 3개 있을 때 S를 받은 경우 S가 홀수개 있으면 N을 여는 위치
+        
+        available_locs = [item for item in available_locs if None not in item]
+
     
+        place, _ = tool.minimax(self.board, self.available_pieces, available_locs, selected_piece, type = 'place')
 
+        return place
 
-'''
-16 16 
-'''
---
+++ tool.py
@@ -0,0 +1,295 @@
+import numpy as np
+
+
+pieces = [(i, j, k, l) for i in range(2) for j in range(2) for k in range(2) for l in range(2)]  # All 16 pieces
+
+def check_win(board):
+    def check_line(line):
+        if 0 in line:
+            return False  # Incomplete line
+        characteristics = np.array([pieces[piece_idx - 1] for piece_idx in line])
+        for i in range(4):  # Check each characteristic (I/E, N/S, T/F, P/J)
+            if len(set(characteristics[:, i])) == 1:  # All share the same characteristic
+                return True
+        return False
+
+    def check_2x2_subgrid_win(board):
+        for r in range(4 - 1):
+            for c in range(4 - 1):
+                subgrid = [board[r][c], board[r][c+1], board[r+1][c], board[r+1][c+1]]
+                if 0 not in subgrid:  # All cells must be filled
+                    characteristics = [pieces[idx - 1] for idx in subgrid]
+                    for i in range(4):  # Check each characteristic (I/E, N/S, T/F, P/J)
+                        if len(set(char[i] for char in characteristics)) == 1:  # All share the same characteristic
+                            return True
+        return False
+    # Check rows, columns, and diagonals
+
+    for col in range(4):
+        if check_line([board[row][col] for row in range(4)]):
+            return True
+    
+    for row in range(4):
+        if check_line([board[row][col] for col in range(4)]):
+            return True
+        
+    if check_line([board[i][i] for i in range(4)]) or check_line([board[i][4 - i - 1] for i in range(4)]):
+        return True
+
+    # Check 2x2 sub-grids
+    if check_2x2_subgrid_win(board):
+        return True
+    
+    return False
+
+def find_checkmate_place(board : list[list], piece : tuple, availplace : list[tuple]) -> tuple:
+    answer = list()
+    for row, col in availplace:
+        board[row][col] = pieces.index(piece) + 1
+        if check_win(board):
+            answer.append((row, col))
+        board[row][col] = 0
+
+    if len(answer) != 0:
+        return answer
+    return False
+
+def find_notcheckmate_piece(board : list[list], availpiece : list[tuple], availplace : list[tuple]) -> list:
+
+    answer = set()
+    
+    for piece in availpiece:
+        for row, col in availplace:
+            board[row][col] = pieces.index(piece) + 1
+            if check_win(board):
+                answer.add(piece)
+                board[row][col] = 0
+                break
+            board[row][col] = 0
+    
+    return list(set(availpiece) - answer)
+
+def find_three(board):
+    def check_three_line(line):
+        # 0~3인덱스의 어떤 원소가 같은지 출력
+
+        '''
+    [[1 1 0 0]
+    [0 1 0 0]
+    [9 9 9 9]
+    [1 1 0 1]]
+    [(1, 1), (2, 0)]
+        '''
+        answer = list()
+        if line.count(0) != 1:
+            return False  # Incomplete line
+        characteristics = np.array([pieces[piece_idx - 1] if piece_idx != 0 else (9, 9, 9, 9) for piece_idx in line])
+        for i in range(4):  # Check each characteristic (I/E, N/S, T/F, P/J)
+            element, counts = np.unique(characteristics[:, i], return_counts = True)
+
+            if np.any(counts == 3):
+                temp = [9, 9, 9, 9]
+                temp[i] = element[counts == 3][0]
+                answer.append([temp])
+        
+        if len(answer) != 0:
+            return answer
+        return False
+
+    def check_three_2x2_subgrid(board):
+        answer = list()
+
+        for r in range(4 - 1):
+            for c in range(4 - 1):
+                subgrid = [board[r][c], board[r][c+1], board[r+1][c], board[r+1][c+1]]
+                if subgrid.count(0) == 1:  # cells fill in 3
+                    characteristics = [pieces[idx - 1] if idx != 0 else (9, 9, 9, 9) for idx in subgrid]
+                    for i in range(4):  # Check each characteristic (I/E, N/S, T/F, P/J)      
+                        for item in set(char[i] for char in characteristics):
+                            if [char[i] for char in characteristics].count(item) == 3:
+
+                                temp = [9, 9, 9, 9]
+                                temp[i] = item
+
+                                x = subgrid.index(0)
+                                a,b = r + x // 2, c + x % 2
+                                answer.append([temp, (a, b)])
+        if len(answer) != 0:
+            return answer
+        
+        return False
+    
+    answer = list()
+
+    for col in range(4):
+        temp = disputelist(check_three_line([board[row][col] for row in range(4)]))
+
+        for t in temp:
+            r = [board[row][col] for row in range(4)].index(0)
+            t.append((r, col))
+            answer.append(t)
+
+    for row in range(4):
+        temp = disputelist(check_three_line([board[row][col] for col in range(4)]))
+        
+        for t in temp:
+            c = [board[row][col] for col in range(4)].index(0)            
+            t.append((row, c))
+            answer.append(t)
+
+
+    temp = disputelist(check_three_line([board[i][i] for i in range(4)]))
+
+    
+    for t in temp:
+        
+        i = [board[i][i] for i in range(4)].index(0)
+        t.append((i, i))
+        answer.append(t)
+
+    temp = disputelist(check_three_line([board[i][4 - i - 1] for i in range(4)]))
+
+    for t in temp:
+        i = [board[i][4 - i - 1] for i in range(4)].index(0)
+        t.append((i, 4 - i - 1))
+        answer.append(t)
+    # Check 2x2 sub-grids
+    temp = disputelist(check_three_2x2_subgrid(board))
+    answer.extend(temp)
+
+    # False 제거 및 모든 리스트 내부의 튜플 풀기
+
+    if len(answer) == 0:
+        return False
+    
+    return answer
+
+def disputelist(ls : list) -> list:
+    if ls:
+        return ls
+    
+    return []
+
+def find_piece_by_point(board : list[list], availpiece : list) -> list[tuple]:
+
+    data = find_three(board)
+    ans = list()
+
+    for point in data:
+        ans.append(find_power_otherattr(point[0], availpiece))
+
+    return ans
+
+def find_power_otherattr(point : tuple, availpiece):
+    answer = 0
+    if 1 in point:
+        p = (point.index(1), 0)
+    else:
+        p = (point.index(0), 1)
+
+    for piece in availpiece:
+        if piece[p[0]] == p[1]:
+            answer += 1
+    
+    return answer
+
+def minimax(board, availpiece, availplace, select_piece = None, alpha = -9999, beta = 9999, flag = 1, type = 'piece') -> list:
+    #P1(flag == 1) win -> 1 else -1
+     
+    if check_win(board):
+        if flag == 1:
+            return None, 1
+        else:
+            return None, -1
+
+    if type == 'piece':
+        return explore_move_piece(board, availpiece, availplace, alpha, beta, flag)
+    else:
+        return explore_move_place(board, availpiece, availplace, select_piece, alpha, beta, flag)
+    
+def explore_move_piece(board, availpiece, availplace, alpha, beta, flag):
+    value = -9999 if flag == 1 else 9999
+    ans = None
+    for piece in availpiece:
+        
+        temp_availpiece = list(filter(lambda x: x != piece, availpiece))
+        for row, col in availplace:
+            board[row][col] = pieces.index(piece) + 1
+            temp_availplace = list(filter(lambda x: x != (row, col), availplace))
+
+            _, score = minimax(
+                board,
+                temp_availpiece,
+                temp_availplace,
+                piece,
+                alpha,
+                beta,
+                flag = 3 - flag,
+                type = 'place'
+            )
+            
+            board[row][col] = 0
+            
+            if flag == 1:
+                if value < score:
+                    ans = piece
+                    value = score
+
+                alpha = max(alpha, value)
+
+            else:
+                if value > score:
+                    ans = piece
+                    value = score
+
+                beta = min(beta, value)
+
+            if beta <= alpha:
+                return ans, value
+    
+    return ans, value
+
+def explore_move_place(board, availpiece, availplace, piece, alpha, beta, flag):
+    value = -9999 if flag == 1 else 9999
+    ans = None
+
+    temp_availpiece = list(filter(lambda x : x != piece, availpiece))
+
+    for row, col in availplace:
+        board[row][col] = pieces.index(piece) + 1
+        temp_availplace = list(filter(lambda x : x != (row, col), availplace))
+            
+        _, score = minimax(
+            board,
+            temp_availpiece,
+            temp_availplace,
+            None,
+            alpha,
+            beta,
+            flag = flag,
+            type = 'piece'
+        )
+
+        board[row][col] = 0
+        
+        if flag == 1:   
+            if value < score:
+                ans = (row, col)
+                value = score
+
+            alpha = max(alpha, value)
+        
+        else:
+            if value > score:
+                ans = (row, col)
+                value = score
+            
+            beta = min(beta, value)
+
+        if beta <= alpha:
+            break
+        
+        return ans, value
+    
+    return ans, value
+
